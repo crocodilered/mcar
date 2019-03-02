b4bec92a6d9178f4bc8eaad80a6ded3e30875867
Serge <serge.zolotukhin@gmail.com>
2019-03-02 20:16:15 +0300

    Refactored slightly.

--- src/components/vehicle/vehicle-notes.vue
+++ src/components/vehicle/vehicle-notes.vue
@@ -1,52 +1,54 @@
 <template>
   <div>
     <template v-if="mode === MODE_LIST">
-      <div class="fab">
-        <button
-          class="mdc-fab"
-          aria-label="Добавить заметку"
-          title="Добавить заметку"
-          @click="showForm"
-        >
-          <span class="mdc-fab__icon material-icons">add</span>
-        </button>
-      </div>
-      <div class="list">
-        <progress-circular
-          v-if="loading"
-        />
-        <ul
-          v-if="list"
-          class="mdc-list mdc-list--two-line"
-        >
-          <li
-            v-for="(o, i) in list"
-            :key="`item-${i}`"
-            class="mdc-list-item"
-            @click="showNote(o)"
+
+      <button
+        class="mdc-fab app-fab--absolute"
+        aria-label="Добавить заметку"
+        title="Добавить заметку"
+        @click="showForm"
+      >
+        <span class="mdc-fab__icon material-icons">add</span>
+      </button>
+
+      <template v-if="loading">
+        <progress-circular/>
+      </template>
+      <template v-else>
+        <div class="list">
+          <ul
+            v-if="list"
+            class="mdc-list mdc-list--two-line"
           >
-            <span class="mdc-list-item__text">
-              <span class="mdc-list-item__primary-text">{{ o.content }}</span>
-              <span class="mdc-list-item__secondary-text">{{ o.date.toLocaleDateString() }}</span>
-            </span>
-          </li>
-        </ul>
-      </div>
+            <li
+              v-for="(o, i) in list"
+              :key="`item-${i}`"
+              class="mdc-list-item"
+              @click="showNote(o)"
+            >
+              <span class="mdc-list-item__text">
+                <span class="mdc-list-item__primary-text">{{ o.content }}</span>
+                <span class="mdc-list-item__secondary-text">{{ o.date.toLocaleDateString() }}</span>
+              </span>
+            </li>
+          </ul>
+          <div
+            v-else
+            style="
+              margin-top: 40%;
+              text-align: center;
+            "
+          >
+            <p style="margin-bottom: 2em;"><big>Данных пока нет</big>,<br> но вы можете добавить первую запись.</p>
+          </div>
+        </div>
+      </template>
     </template>
     <template v-if="mode === MODE_FORM">
       <div class="form">
-        <h2>
-          <button
-            @click="showList"
-            class="mdc-icon-button"
-            aria-label="Вернуться"
-            aria-hidden="true"
-            aria-pressed="false"
-          >
-            <i class="material-icons mdc-icon-button__icon">arrow_back</i>
-          </button>
-          Новая заметка
-        </h2>
+        <h1>
+          Добавить заметку
+        </h1>
         <mdc-textarea
           label="Заметка"
           v-model="form.content"
@@ -103,10 +105,16 @@
         MODE_FORM: 'form',
         MODE_NOTE: 'note',
         mode: 'list',
-        list: [],
+        list: null,
         note: null,
         form: null,
         loading: false
+      }
+    },
+
+    watch: {
+      vehicle () {
+        this.loadNotes()
       }
     },
 
@@ -120,6 +128,15 @@
     },
 
     methods: {
+      async loadNotes () {
+        this.loading = true
+        const snapshot = await this.notesRef.orderBy('date', 'desc').get()
+        this.list = snapshot.empty
+          ? null 
+          : snapshot.docs.map(doc => new NoteModel(doc.data()))
+        this.loading = false
+      },
+
       showList () {
         this.mode = this.MODE_LIST
       },
@@ -162,12 +179,7 @@
     },
 
     async created () {
-      this.loading = true
-      // this.notes = await this.notesRef.orderBy('date').get()
-      const querySnapshot = await this.notesRef.orderBy('date', 'desc').get()
-      this.loading = false
-      this.list = []
-      querySnapshot.forEach(doc => this.list.push(new NoteModel(doc.data())))
+      await this.loadNotes()
     }
   }
 </script>
@@ -177,15 +189,7 @@
   @import "@material/fab/mdc-fab";
   @import "@material/icon-button/mdc-icon-button";
 
-  .fab {
-    margin-bottom: 2em;
-  }
-
-  @media (max-width: 500px) {
-    .fab {
-      position: absolute;
-      right: 5%;
-      bottom: 0;
-    }
+  .mdc-list li {
+    cursor: pointer;
   }
 </style>
