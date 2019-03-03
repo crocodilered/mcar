0ce1a6dcb694488d2cba2762178dd1bded80c3e3
Serge <serge.zolotukhin@gmail.com>
2019-03-03 15:48:26 +0300

    PWA intro: sw & a2hs implemented.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const PUBLIC_PATH = 'https://www.my-car.com/';
+
 module.exports = {
+  runtimeCompiler: true,
+
   css: {
     loaderOptions: {
       sass: {
@@ -6,5 +11,19 @@ module.exports = {
       }
     }
   },
-  runtimeCompiler: true
+
+  configureWebpack: {
+    plugins: [
+      new SWPrecacheWebpackPlugin (
+        {
+          cacheId: 'my-car',
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: 'service-worker.js',
+          minify: true,
+          navigateFallback: PUBLIC_PATH + 'index.html',
+          staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+        }
+      )
+    ]
+  }
 }
