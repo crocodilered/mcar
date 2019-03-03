ef7354144ffa7f3b1cb47a58e96ee42e2c282cba
Serge <serge.zolotukhin@gmail.com>
2019-03-04 00:13:42 +0300

    Init.

---
+++ src/components/common/overlay.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form window-overlay">
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+          <div style="text-align: right;">
+            <mdc-icon-button
+              @click="$emit('close')"
+              title="Закрыть"
+            >
+              close
+            </mdc-icon-button>
+          </div>
+          <h1>{{ title }}</h1>
+          <slot/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MdcIconButton from '@/components/mdc/icon-button'
+  export default {
+    components: { MdcIconButton },
+    name: 'Overlay',
+    props: ['title']
+  }
+</script>
+
+<style>
+
+</style>
