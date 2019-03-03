59ef211e6e0081f367007c5497d363341f86a3ab
Serge <serge.zolotukhin@gmail.com>
2019-03-03 23:21:26 +0300

    Init.

---
+++ src/components/mdc/button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    ref='button'
+    :disabled="loading"
+    class="mdc-button mdc-button--unelevated mdc-button--extended"
+    @click="$emit('click')"
+  >
+    <i
+      v-if="icon"
+      class="material-icons mdc-button__icon"
+    >
+      {{ icon }}
+    </i>
+    <span class="mdc-button__label">
+      <slot/>
+    </span>
+  </button>
+</template>
+
+<script>
+  import {MDCRipple} from '@material/ripple'
+
+  export default {
+    name: 'MdcButton',
+    props: ['loading', 'icon'],
+
+    mounted () {
+      this.button = new MDCRipple(this.$refs.button)
+    },
+
+    destroyed () {
+      this.button.destroy()
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "@/assets/variables.scss";
+  @import "~@material/button/mdc-button";
+
+  @media (max-width: $width-sm) {
+    .mdc-button--extended {
+      padding: 15px 20px;
+      height: auto;
+    }
+  }
+</style>
---
+++ src/components/mdc/icon-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    ref='button'
+    class="mdc-icon-button material-icons"
+    @click="$emit('click')"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script>
+  import {MDCRipple} from '@material/ripple'
+
+  export default {
+    name: 'MdcIconButton',
+
+    mounted () {
+      this.button = new MDCRipple(this.$refs.button)
+      this.button.unbounded = true
+    },
+
+    destroyed () {
+      this.button.destroy()
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "@/assets/variables.scss";
+  @import "~@material/icon-button/mdc-icon-button";
+</style>
